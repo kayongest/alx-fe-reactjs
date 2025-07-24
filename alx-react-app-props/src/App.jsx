f13e68df838b730b6de0bd30ec6705c4262662c6
kayongest <kayonga70@gmail.com>
2025-07-24 21:58:47 +0200

    Latest Updates

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,10 +1,3 @@
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import UserProfile from './components/UserProfile.jsx';
-import WelcomeMessage from './components/WelcomeMessage';
-import './App.css'
-import UserContext from './UserContext';
 import ProfilePage from './components/ProfilePage';
 
 function App() {
@@ -13,4 +6,4 @@ function App() {
   return <ProfilePage userData={userData} />;
 }
 
-export default App
+export default App;
