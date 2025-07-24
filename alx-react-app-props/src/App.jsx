9623065102d04c2dacfebbc0839e5a2741130fcc
kayongest <kayonga70@gmail.com>
2025-07-24 21:34:21 +0200

    Refactor UserContext import statements for consistency

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -7,7 +7,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { UserContext } from './UserContext';
+import UserContext from './UserContext';
 
 function App() {
   const [count, setCount] = useState(0)
