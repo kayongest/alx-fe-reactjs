fc17698036d3bbfa3368cf9d8c9cb20c2c4772fc
kayongest <kayonga70@gmail.com>
2025-07-26 22:51:39 +0200

    Enhance recipe store with update and delete functionalities

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,7 +3,15 @@ import { create } from 'zustand'
 const useRecipeStore = create(set => ({
   recipes: [],
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes })
+  setRecipes: (recipes) => set({ recipes }),
+  updateRecipe: (recipeId, updatedRecipe) => set(state => ({
+    recipes: state.recipes.map(recipe => 
+      recipe.id === recipeId ? { ...recipe, ...updatedRecipe } : recipe
+    )
+  })),
+  deleteRecipe: (recipeId) => set(state => ({
+    recipes: state.recipes.filter(recipe => recipe.id !== recipeId)
+  }))
 }))
 
 export default useRecipeStore; 
