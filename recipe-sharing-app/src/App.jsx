d2e50bfa1cc0f9bae1bb6f6b62e2bac9d2d81b27
kayongest <kayonga70@gmail.com>
2025-07-27 10:40:34 +0200

    Latest update

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,27 +2,26 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBar from './components/SearchBar';
 import AddRecipeForm from './components/AddRecipeForm';
 import AdvancedFilters from './components/AdvancedFilters';
+import RecipeList from './components/RecipeList';
+import RecipeDetail from './components/RecipeDetail';
 import './App.css';
-
-// Simple test component
-const TestComponent = () => {
-  return (
-    <div>
-      <h2>Test Component Works!</h2>
-      <p>If you can see this, React is working.</p>
-    </div>
-  );
-};
 
 function App() {
   return (
     <Router>
       <div className="app-container">
-        <h1>Recipe Sharing App</h1>
-        <TestComponent />
-        <AddRecipeForm />
-        <SearchBar />
-        <AdvancedFilters />
+        <Routes>
+          <Route path="/" element={
+            <>
+              <h1>Recipe Sharing App</h1>
+              <AddRecipeForm />
+              <SearchBar />
+              <AdvancedFilters />
+              <RecipeList />
+            </>
+          } />
+          <Route path="/recipe/:recipeId" element={<RecipeDetail />} />
+        </Routes>
       </div>
     </Router>
   );
