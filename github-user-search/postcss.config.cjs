e50dbad5a13618f1cdad5c2cecf43267302bdba0
kayongest <kayonga70@gmail.com>
2025-08-10 21:08:16 +0200

    Installed both Tailwindcss & Postcss

---
+++ github-user-search/postcss.config.cjs
@@ -0,0 +1,8 @@
+// This is the script for postcss.config.cjs
+// This file is used to configure Tailwind CSS in this project
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
---
+++ github-user-search/tailwind.config.cjs
@@ -0,0 +1,13 @@
+// This is the script for tailwind.config.cjs
+// This file is used to configure Tailwind CSS in this project
+// This specifies the content paths and theme extensions for Tailwind CSS
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
