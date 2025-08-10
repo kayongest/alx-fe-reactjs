36f576a6353ae97c7050f66dea813fa0acaae88e
kayongest <kayonga70@gmail.com>
2025-08-10 21:51:09 +0200

    Updated tailwindcss - included ./styles.css in content

--- github-user-search/src/main.jsx
+++ github-user-search/src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import './index.css';  // Tailwind directives
+import './styles.css'; // My custom styles
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
--- github-user-search/tailwind.config.cjs
+++ github-user-search/tailwind.config.cjs
@@ -4,7 +4,8 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    "./public/index.html"
+    "./public/index.html",
+    "./src/styles.css"
   ],
   theme: {
     extend: {
